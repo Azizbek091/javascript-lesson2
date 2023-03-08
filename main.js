42d39700d0038d51159f55697c62b7fbc4d0c621
Aziz <19191@.ru>
2023-03-08 18:21:40 +0500

    save

---
+++ main.js
@@ -0,0 +1,12 @@
+var first = +prompt('Введите первое число');
+console.log(first);
+
+var second = +prompt('Введите второе число');
+console.log(second);
+
+var third = +prompt('Введите третье число');
+console.log(third);
+
+var res = (first + second + third) / 3 
+
+alert('Среднее арифмитическое: ' + res)
